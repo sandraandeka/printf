8fb4b633ba4a2dd60c40d7edb21179e39d79ea04
sandra andeka <sandraandex13@gmail.com>
2022-04-20 08:18:10 -0700

    added

--- main.c
+++ main.c
@@ -10,7 +10,7 @@ int main(void)
 { 
 	int len;
 	int len2;
-	unsingned int ui;
+	unsigned int ui;
 	void *addr;
 	
     len = _printf("Let's try to printf a simple sentence.\n");
@@ -26,8 +26,8 @@ int main(void)
     _printf("Unsigned octal:[%o]\n", ui);
     printf("Unsigned octal:[%o]\n", ui);
     _printf("Unsigned hexadecimal:[%x, %X]\n", ui, ui);
-    printf("Unsigned hexadecimal:[%x, %X]\n", ui, ui);
-    _printf("Character:[%c]\n", 'H');
+    printf(i"Unsigned hexadecimal:[%x, %X]\n", ui, ui);
+    _printf(iiiiii"Character:[%c]\n", 'H');
     printf("Character:[%c]\n", 'H');
     _printf("String:[%s]\n", "I am a string !");
     printf("String:[%s]\n", "I am a string !");
