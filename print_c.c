aaec1056759301030b2912daf7e3ae137e8aa5e6
sandra andeka <sandraandex13@gmail.com>
2022-04-20 16:37:05 -0700

    added new files

---
+++ print_c.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * print_c - function that prints a char
+ * @c: character to print
+ * Return:Always 1 (success)
+ */
+int print_c(va_list c)
+{
+	char character = (char)va_arg(c, int);
+
+	_putchar(character);
+	return (1);
+}
